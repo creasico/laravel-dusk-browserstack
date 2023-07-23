4192b1479677dd682bcf988c77967303070eb092
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-07-23 22:54:19 +0700

    chore: make it testable
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ tests/ServiceProviderTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Creasi\Tests;
+
+use Facebook\WebDriver\Exception\TimeoutException;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Dusk\Browser;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Test;
+use SebastianBergmann\CodeCoverage\Driver\Driver;
+
+#[Group('serviceProvider')]
+class ServiceProviderTest extends TestCase
+{
+    use WithFaker;
+
+    #[Test]
+    #[Group('dusk')]
+    public function should_adds_dusk_macros()
+    {
+        $this->assertTrue(Browser::hasMacro('waitForInertia'));
+    }
+
+    #[Test]
+    #[Group('dusk')]
+    public function should_throws_an_exception_if_the_count_doesnt_increase()
+    {
+        $driver = $this->mock(Driver::class);
+
+        $driver->shouldReceive('executeScript')
+            ->with('return window.__inertiaNavigatedCount;')
+            ->once()
+            ->andReturn(0);
+
+        $driver->shouldReceive('executeScript')
+            ->with('return window.__inertiaNavigatedCount > 0;')
+            ->andReturn(0);
+
+        $browser = new Browser($driver);
+
+        try {
+            $browser->waitForInertia(0.1); // wait for 0.1 seconds
+        } catch (TimeoutException $e) {
+            return $this->assertTrue(true);
+        }
+
+        $this->fail('Should have thrown TimeoutException');
+    }
+
+    #[Test]
+    #[Group('dusk')]
+    public function should_passes_when_the_count_increases()
+    {
+        $driver = $this->mock(Driver::class);
+
+        $driver->shouldReceive('executeScript')
+            ->with('return window.__inertiaNavigatedCount;')
+            ->once()
+            ->andReturn(0);
+
+        $driver->shouldReceive('executeScript')
+            ->with('return window.__inertiaNavigatedCount > 0;')
+            ->once()
+            ->andReturn(1);
+
+        $browser = new Browser($driver);
+        $browser->waitForInertia(1);
+
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Creasi\Tests;
+
+use Creasi\DuskBrowserstack\ServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+    /**
+     * @param  \Illuminate\Foundation\Application  $app
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [
+            ServiceProvider::class,
+        ];
+    }
+
+    /**
+     * @param  \Illuminate\Foundation\Application  $app
+     */
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app->useEnvironmentPath(\dirname(__DIR__));
+    }
+}
