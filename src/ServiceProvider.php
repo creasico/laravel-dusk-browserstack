727235cb5bdb89d4718839aa16d67dbadf9e9476
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-07-23 23:01:36 +0700

    feat: register `laravel-dusk` macro for inertia navigation event
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ src/ServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Creasi\DuskBrowserstack;
+
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+use Laravel\Dusk\Browser;
+
+class ServiceProvider extends IlluminateServiceProvider
+{
+    public function boot(): void
+    {
+        // .
+    }
+
+    public function register(): void
+    {
+        if (app()->environment('testing') && \class_exists(Browser::class)) {
+            $this->registerDuskMacroForInertia();
+        }
+    }
+
+    /**
+     * Register inertia.js helper for dusk testing
+     *
+     * @see https://github.com/protonemedia/inertiajs-events-laravel-dusk
+     */
+    private function registerDuskMacroForInertia(): void
+    {
+        if (! \class_exists(\Inertia\Inertia::class)) {
+            return;
+        }
+
+        Browser::macro('waitForInertia', function (int $seconds = null): Browser {
+            /** @var Browser $this */
+            $driver = $this->driver;
+
+            $currentCount = $driver->executeScript('return window.__inertiaNavigatedCount;');
+
+            return $this->waitUsing($seconds, 100, fn () => $driver->executeScript(
+                "return window.__inertiaNavigatedCount > {$currentCount};"
+            ), 'Waited %s seconds for Inertia.js to increase the navigate count.');
+        });
+    }
+}
